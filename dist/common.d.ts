3a6c2ffa2d050d1a012de6fa38038c63005863b5
LuK1337 <priv.luk@gmail.com>
2026-05-19 19:45:31 +0200

    Kill dist

--- dist/common.d.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Reads all of the data in the given blob and returns it as an ArrayBuffer.
- *
- * @param {Blob} blob - Blob with the data to read.
- * @returns {Promise<ArrayBuffer>} ArrayBuffer containing data from the blob.
- * @ignore
- */
-export declare function readBlobAsBuffer(blob: Blob): Promise<ArrayBuffer>;
-//# sourceMappingURL=common.d.ts.map
--- dist/index.d.ts
+++
@@ -1,7 +0,0 @@
-export { FastbootDevice } from "./fastboot";
-export { FastbootError, ImageError, LpError, TimeoutError, UsbError, } from "./utils/errors";
-export { USER_ACTION_MAP } from "./factory";
-export { setDebugLevel, setDebugLogger } from "./utils/logger";
-export { type LpMetadata, type LpMetadataGeometry, type LpMetadataBlockDevice, readFromImageBlob, getMetadataSuperBlockDevice, getBlockDevicePartitionName, buildWipeSuperImages, } from "./lp";
-export { configure as configureZip } from "@zip.js/zip.js";
-//# sourceMappingURL=index.d.ts.map
